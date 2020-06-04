df450c2e13c2cc112c076958124f6730f82a12bb
Jongmin Park <jmpark93@koscom.co.kr>
2020-06-04 17:17:07 +0900

    update ...

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -2,10 +2,10 @@
   <div class="hello">
     <h3>{{ msg }}</h3>
     <p>
-      Cloud Foundry 의 Static Web 제작 설명을 위한 샘플 프로그램입니다. 자세한 설명은 다음 링크를 참조하시기 바랍니다. 
+      Cloud Foundry 를 활용한 전통적인 웹 서비스(WEB >> WAS >> DB) 제작 설명을 위한 샘플 프로그램입니다. 자세한 설명은 다음 링크를 참조하시기 바랍니다. 
     </p>
     <p>
-      <a href="https://docs.kpaasta.io/dev/static" target="_blank" rel="noopener"> 정적 웹 앱 제작가이드 </a>
+      <a href="https://docs.kpaasta.io/dev/legacy" target="_blank" rel="noopener"> 전통적인 웹 서비스 제작가이드 </a>
     </p>
   </div>
 </template>
